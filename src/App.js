a678a8ab890caf0b497692722ae13ad6fbf087f2
cheruto-yego <yegohosana@gmail.com>
2024-01-29 15:54:13 +0300

    import logo  removed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
@@ -11,30 +11,46 @@ import { useState } from "react";
 import { AppContext, socket } from "./context/appContext";
 
 function App() {
-    const [rooms, setRooms] = useState([]);
-    const [currentRoom, setCurrentRoom] = useState([]);
-    const [members, setMembers] = useState([]);
-    const [messages, setMessages] = useState([]);
-    const [privateMemberMsg, setPrivateMemberMsg] = useState({});
-    const [newMessages, setNewMessages] = useState({});
-    const user = useSelector((state) => state.user);
-    return (
-        <AppContext.Provider value={{ socket, currentRoom, setCurrentRoom, members, setMembers, messages, setMessages, privateMemberMsg, setPrivateMemberMsg, rooms, setRooms, newMessages, setNewMessages }}>
-            <BrowserRouter>
-                <Navigation />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    {!user && (
-                        <>
-                            <Route path="/login" element={<Login />} />
-                            <Route path="/signup" element={<Signup />} />
-                        </>
-                    )}
-                    <Route path="/chat" element={<Chat />} />
-                </Routes>
-            </BrowserRouter>
-        </AppContext.Provider>
-    );
+  const [rooms, setRooms] = useState([]);
+  const [currentRoom, setCurrentRoom] = useState([]);
+  const [members, setMembers] = useState([]);
+  const [messages, setMessages] = useState([]);
+  const [privateMemberMsg, setPrivateMemberMsg] = useState({});
+  const [newMessages, setNewMessages] = useState({});
+  const user = useSelector((state) => state.user);
+  return (
+    <AppContext.Provider
+      value={{
+        socket,
+        currentRoom,
+        setCurrentRoom,
+        members,
+        setMembers,
+        messages,
+        setMessages,
+        privateMemberMsg,
+        setPrivateMemberMsg,
+        rooms,
+        setRooms,
+        newMessages,
+        setNewMessages,
+      }}
+    >
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {!user && (
+            <>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </>
+          )}
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
+    </AppContext.Provider>
+  );
 }
 
 export default App;
